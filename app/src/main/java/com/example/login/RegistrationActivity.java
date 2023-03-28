97f20d9021fb4115abe4c45ef0855aa688f627fb
Zawalisz <marcin.zawalisz@gmail.com>
2023-03-28 20:21:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/login/RegistrationActivity.java
@@ -0,0 +1,31 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        EditText login = (EditText) findViewById(R.id.editTextTextPersonName2);
+        EditText pwd = (EditText) findViewById(R.id.editTextTextPassword);
+
+        Button registrationBut = (Button) findViewById(R.id.button3);
+
+        registrationBut.setOnClickListener(view -> {
+            String loginValue = login.getText().toString();
+            String pwdValue = pwd.getText().toString();
+            Intent intentRegestration = new Intent(this, MainActivity.class);
+            intentRegestration.putExtra("login", loginValue);
+            intentRegestration.putExtra("password", pwdValue);
+            startActivity(intentRegestration);
+        });
+    }
+}
